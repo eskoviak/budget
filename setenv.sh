c56789436d6fc94049bbcd6ab7e641bb2d08cf70
Ed Skoviak <eskoviak@gmail.com>
2022-01-17 18:59:22 -0600

    updating

--- setenv.sh
+++
@@ -1,5 +0,0 @@
-export COCKROACHDBURL="postgresql://ed:Kh4V3R9B7DcygecH@free-tier.gcp-us-central1.cockroachlabs.cloud:26257/bank?sslmode=verify-full&sslrootcert=/Users/edmundlskoviak/.postgresql/root.crt&options=--cluster%3Dgolden-dingo-2123"
-
-export PROMPT='(%F{green}psql%f) %# '
-
-alias cpsql="cockroach sql --url $COCKROACHDBURL"
